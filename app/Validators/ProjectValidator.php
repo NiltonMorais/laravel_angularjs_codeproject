452d11cb7ba7a61728063573c29b707150da1501
Nilton Morais <niltonmorais123@gmail.com>
2016-01-19 17:01:55 -0300

    corrigindo Validator ProjectValidator a pedido do Bruno

--- app/Validators/ProjectValidator.php
+++ app/Validators/ProjectValidator.php
@@ -8,10 +8,11 @@ class ProjectValidator extends LaravelValidator
     protected $rules = [
         'owner_id' => 'required|integer',
         'client_id' => 'required|integer',
-        'name' => 'required',
-        'status' => 'required',
-        'progress' => 'required',
-        'due_date' => 'required',
+        'name' => 'required|max:255',
+        'description' => 'max:255',
+        'status' => 'required|integer|between:1,100',
+        'progress' => 'required|integer|between:1,100',
+        'due_date' => 'required|date',
     ];
 
 }
